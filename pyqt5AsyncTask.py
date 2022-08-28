072f669814b8debaa91996532efb6781ee779cb6
yinkaisheng <yinkaisheng@agora.io>
2022-08-28 22:55:45 +0800

    update

--- pyqt5AsyncTask.py
+++ pyqt5AsyncTask.py
@@ -20,10 +20,12 @@ class MyThread(QThread):
         self.func = func
         self.args = args
         self.notifyWhenExit = notifyWhenExit
+        self.debugPrint = True
 
     def run(self) -> None:
         sysThreadId = threading.get_ident()
-        print('task id {} runs in thread {}'.format(self.taskId, sysThreadId))
+        if self.debugPrint:
+            print('task id {} runs in thread {}'.format(self.taskId, sysThreadId))
         result = self.func(self.Signal, self.taskId, self.args)
         if self.notifyWhenExit:
             self.Signal.emit((self.taskId, MsgIDThreadExit, result))
@@ -32,26 +34,27 @@ class MyThread(QThread):
 class AsyncTask():
     def __init__(self):
         super().__init__()
-        self._taskThreads = {}
-        self._taskNotifiers = {}
-        self._taskId = 0
-        self._msgIdName = {MsgIDThreadExit: 'MsgIDThreadExit'}
+        self.taskThreads = {}
+        self.taskNotifiers = {}
+        self.taskId = 0
+        self.msgIdName = {MsgIDThreadExit: 'MsgIDThreadExit'}
         self.delayCallTimers = {}
+        self.debugPrint = True
 
-    def delayCall(self, timeMs: int, func: Callable[[None], None]) -> None:
+    def delayCall(self, timeMs: int, func: Callable[[Any], None], *args, **kwargs) -> None:
         timer = QTimer()
         timer.setSingleShot(True)
         timer.timeout.connect(self.onDelayCallTimer)
         timer.start(timeMs)
-        self.delayCallTimers[timer] = func
+        self.delayCallTimers[timer] = (func, args, kwargs)
 
     def onDelayCallTimer(self) -> None:
         timer = self.sender()
-        func = self.delayCallTimers.pop(timer)
-        func()
+        func, args, kwargs = self.delayCallTimers.pop(timer)
+        func(*args, **kwargs)
 
     def setMsgIDName(self, msgId: int, name: str) -> None:
-        self._msgIdName[msgId] = name
+        self.msgIdName[msgId] = name
 
     def runTaskInThread(self, taskFunc: Callable[[pyqtSignal, int, Any], Any], args: Any, notifyFunc: Callable[[int, int, Any], None]) -> int:
         """
@@ -64,45 +67,49 @@ class AsyncTask():
           then notifyFunc will be called in caller thread that spawned the new thread
         return int, a task id that starts with 1, corresponds to the new thread
         """
-        self._taskId += 1
-        thread = MyThread(self._taskId, taskFunc, args)
+        self.taskId += 1
+        thread = MyThread(self.taskId, taskFunc, args)
+        thread.debugPrint = self.debugPrint
         thread.Signal.connect(self._threadSlot)
-        self._taskThreads[self._taskId] = thread
-        self._taskNotifiers[self._taskId] = notifyFunc
+        self.taskThreads[self.taskId] = thread
+        self.taskNotifiers[self.taskId] = notifyFunc
         sysThreadId = threading.get_ident()
-        print('caller thread id {} spawns task id {}'.format(sysThreadId, self._taskId))
+        if self.debugPrint:
+            print('caller thread id {} spawns task id {}'.format(sysThreadId, self.taskId))
         thread.start()
-        return self._taskId
+        return self.taskId
 
     def taskIsRunning(self) -> bool:
-        return bool(self._taskThreads)
+        return bool(self.taskThreads)
 
     def taskCount(self) -> int:
-        return len(self._taskThreads)
+        return len(self.taskThreads)
 
     def addExtraCallback(self, callbackFunc: Callable[[], None], callbackArgv: Any = None) -> None:
-        for taskId in self._taskNotifiers:
-            if isinstance(self._taskNotifiers[taskId], list):
-                self._taskNotifiers[taskId].append((callbackFunc, callbackArgv))
+        for taskId in self.taskNotifiers:
+            if isinstance(self.taskNotifiers[taskId], list):
+                self.taskNotifiers[taskId].append((callbackFunc, callbackArgv))
             else:
-                singleTask = self._taskNotifiers[taskId]
-                self._taskNotifiers[taskId] = [singleTask, (callbackFunc, callbackArgv)]
+                singleTask = self.taskNotifiers[taskId]
+                self.taskNotifiers[taskId] = [singleTask, (callbackFunc, callbackArgv)]
 
     def _threadSlot(self, atuple: Tuple[int, int, Any]) -> None:
         taskId, msgId, args = atuple
         sysThreadId = threading.get_ident()
-        print('slot runs in thread id {}, signal emitted from task id {}, msg id {}[{}]'.format(
-            sysThreadId, taskId, msgId, self._msgIdName.get(msgId, '?')))
-        if isinstance(self._taskNotifiers[taskId], list):
-            self._taskNotifiers[taskId][0](taskId, msgId, args)
-            for func, argv in self._taskNotifiers[taskId][1:]:  # [1:] are callbacks added by addExtraCallback
-                print('----call extra callback----')
+        if self.debugPrint:
+            print('slot runs in thread id {}, signal emitted from task id {}, msg id {}[{}]'.format(
+                sysThreadId, taskId, msgId, self.msgIdName.get(msgId, '?')))
+        if isinstance(self.taskNotifiers[taskId], list):
+            self.taskNotifiers[taskId][0](taskId, msgId, args)
+            for func, argv in self.taskNotifiers[taskId][1:]:  # [1:] are callbacks added by addExtraCallback
+                if self.debugPrint:
+                    print('----call extra callback----')
                 func(argv)
         else:
-            self._taskNotifiers[taskId](taskId, msgId, args)
+            self.taskNotifiers[taskId](taskId, msgId, args)
         if msgId == MsgIDThreadExit:
             # avoid QThread: Destroyed while thread is still running
-            while self._taskThreads[taskId].isRunning():
+            while self.taskThreads[taskId].isRunning():
                 time.sleep(0.005)
-            del self._taskThreads[taskId]
-            del self._taskNotifiers[taskId]
+            del self.taskThreads[taskId]
+            del self.taskNotifiers[taskId]
